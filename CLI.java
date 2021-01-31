809b97ec819abfd8da3658741b24076172f99973
Amélia SZK <amelia.szk@protonmail.com>
2021-01-30 19:42:34 -0500

    Autoformatting

--- CLI.java
+++ CLI.java
@@ -7,8 +7,10 @@ import java.util.List;
 public class CLI {
     public static final String DIAGONALS_OPTION = "--dont-relate-diagonals";
     public static final boolean DIAGONALS_DEFAULT = false;
-    public static final String ERRMSG_DIAGONALS = "Error: With $d arguments, the 1st must be `%s`\n";
-    public static final String ERRMSG_NBARGS = "Error: The program must be called with either 2 or 3 arguments.\n";
+    public static final String ERRMSG_DIAGONALS =
+        "Error: With $d arguments, the 1st must be `%s`\n";
+    public static final String ERRMSG_NBARGS =
+        "Error: The program must be called with either 2 or 3 arguments.\n";
 
     public static void main(String args[]) {
 
@@ -24,25 +26,26 @@ public class CLI {
 
         List<String> argv = Arrays.asList(args);
         switch (argv.size()) {
-            case 3:
-                if (argv.get(0).equals(DIAGONALS_OPTION)) {
-                    dontRelateDiagonals = true;
-                } else {
-                    System.err.printf(ERRMSG_DIAGONALS, argv.size(), DIAGONALS_OPTION);
-                    System.exit(-1);
-                }
-                /* FALL THROUGH */
-            case 2:
-                int lastArgIndex = argv.size() - 1;
-                int penultimateArgIndex = lastArgIndex - 1;
-                imageFilepath = argv.get(penultimateArgIndex);
-                resultsFilepath = argv.get(lastArgIndex);
-                break;
-
-            default:
-                System.err.printf(ERRMSG_NBARGS);
+        case 3:
+            if (argv.get(0).equals(DIAGONALS_OPTION)) {
+                dontRelateDiagonals = true;
+            } else {
+                System.err.printf(ERRMSG_DIAGONALS, argv.size(),
+                                  DIAGONALS_OPTION);
                 System.exit(-1);
-                break;
+            }
+            /* FALL THROUGH */
+        case 2:
+            int lastArgIndex = argv.size() - 1;
+            int penultimateArgIndex = lastArgIndex - 1;
+            imageFilepath = argv.get(penultimateArgIndex);
+            resultsFilepath = argv.get(lastArgIndex);
+            break;
+
+        default:
+            System.err.printf(ERRMSG_NBARGS);
+            System.exit(-1);
+            break;
         }
         System.out.println("dontRelateDiagonals = " + dontRelateDiagonals);
         System.out.println("imageFilepath = " + imageFilepath);
