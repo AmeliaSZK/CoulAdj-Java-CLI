0f95671852c114c33dfd80aae30433e4c0ff5892
Amélia SZK <amelia.szk@protonmail.com>
2021-01-30 20:30:29 -0500

    Main() almost complete

--- CLI.java
+++ CLI.java
@@ -1,16 +1,29 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import javax.imageio.ImageIO;
 
 public class CLI {
     public static final String DIAGONALS_OPTION = "--dont-relate-diagonals";
     public static final boolean DIAGONALS_DEFAULT = false;
+
     public static final String ERRMSG_DIAGONALS =
         "Error: With $d arguments, the 1st must be `%s`\n";
     public static final String ERRMSG_NBARGS =
         "Error: The program must be called with either 2 or 3 arguments.\n";
+    public static final String ERRMSG_IMAGE_FILE_CANREAD =
+        "Error: The file `%s` cannot be read.\n";
+    public static final String ERRMSG_RESULTS_FILE_CANWRITE =
+        "Error: The file `%s` cannot be created or modified.\n";
+    public static final String ERRMSG_IMAGE_NULL =
+        "Error: The file `%s` couldn't be parsed as an image.\n";
 
     public static void main(String args[]) {
 
@@ -52,12 +65,56 @@ public class CLI {
         System.out.println("resultsFilepath = " + resultsFilepath);
 
         // # Open the files
+        File imageFile = new File(imageFilepath);
+        File resultsFile = new File(resultsFilepath);
+
+        if (!imageFile.canRead()) {
+            System.err.printf(ERRMSG_IMAGE_FILE_CANREAD,
+                              imageFile.getAbsolutePath());
+            System.exit(-1);
+        }
+
+        try {
+            resultsFile.createNewFile();
+        } catch (IOException e) {
+            System.err.println(e.getLocalizedMessage());
+            System.exit(-1);
+        }
+
+        if (!resultsFile.canWrite()) {
+            System.err.printf(ERRMSG_RESULTS_FILE_CANWRITE,
+                              resultsFile.getAbsolutePath());
+            System.exit(-1);
+        }
 
         // # Convert to BufferedImage
+        BufferedImage image = null;
+        try {
+            image = ImageIO.read(imageFile);
+        } catch (IOException e) {
+            System.err.println(e.getLocalizedMessage());
+            System.exit(-1);
+        }
+        System.out.println(image);
+
+        if (image == null) {
+            System.err.printf(ERRMSG_IMAGE_NULL, imageFile);
+            System.exit(-1);
+        }
 
         // # Invoke ColourAdjacencies
 
         // # Write results to file
+        List<String> lines = new ArrayList<String>();
+        lines.add("Hello");
+        lines.add("Hi");
+
+        try {
+            Files.write(resultsFile.toPath(), lines);
+        } catch (IOException e) {
+            System.err.println(e.getLocalizedMessage());
+            System.exit(-1);
+        }
 
         // # End timer and report duration
         LocalDateTime endTime = LocalDateTime.now();
