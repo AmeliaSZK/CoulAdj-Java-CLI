09aec344e7e0c4e737ae680a3e1fb185ed2a870b
Amélia SZK <amelia.szk@protonmail.com>
2021-01-30 21:36:22 -0500

    Added comparators

--- ColourAdjacencies.java
+++ ColourAdjacencies.java
@@ -1,7 +1,12 @@
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class ColourAdjacencies {
     public static final boolean DIAGONALS_DEFAULT = false;
@@ -9,6 +14,8 @@ public class ColourAdjacencies {
     private BufferedImage image;
     private boolean dontRelateDiagonals;
     private List<String> lines;
+    private ColorModel cm;
+    private Map<Integer, Set<Integer>> adjacencies;
 
     private boolean adjacenciesAreComputed = false;
     private boolean linesAreComputed = false;
@@ -16,11 +23,13 @@ public class ColourAdjacencies {
     public ColourAdjacencies(BufferedImage image) {
         this.image = image;
         this.dontRelateDiagonals = DIAGONALS_DEFAULT;
+        this.cm = image.getColorModel();
     }
 
     public ColourAdjacencies(BufferedImage image, boolean dontRelateDiagonals) {
         this.image = image;
         this.dontRelateDiagonals = dontRelateDiagonals;
+        this.cm = image.getColorModel();
     }
 
     public void computeAdjacencies() {
@@ -53,5 +62,36 @@ public class ColourAdjacencies {
             computeLines();
         }
         return lines;
+    }
+
+    class SortByRgb implements Comparator<Integer> {
+        public int compare(Integer a, Integer b) {
+            if (cm.getRed(a) != cm.getRed(b)) {
+                return cm.getRed(a) - cm.getRed(b);
+
+            } else if (cm.getGreen(a) != cm.getGreen(b)) {
+                return cm.getGreen(a) - cm.getGreen(b);
+
+            } else {
+                return cm.getBlue(a) - cm.getBlue(b);
+            }
+        }
+    }
+
+    class SortByRgbAlpha implements Comparator<Integer> {
+        public int compare(Integer a, Integer b) {
+            if (cm.getRed(a) != cm.getRed(b)) {
+                return cm.getRed(a) - cm.getRed(b);
+
+            } else if (cm.getGreen(a) != cm.getGreen(b)) {
+                return cm.getGreen(a) - cm.getGreen(b);
+
+            } else if (cm.getBlue(a) != cm.getBlue(b)) {
+                return cm.getBlue(a) - cm.getBlue(b);
+                
+            } else {
+                return cm.getAlpha(a) - cm.getAlpha(b);
+            }
+        }
     }
 }
